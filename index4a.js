20b958bcad7ac4ecea81d7c9c8071fce4484165a
Aryaeet12 <aryajeetkumar34@gmail.com>
2026-02-19 13:50:45 +0530

    update

--- index4a.js
+++ index4a.js
@@ -3,7 +3,7 @@ const app=express()
 app.listen(8080,()=>{
     console.log("Server Started")
 })
-app.get("/:id/:er",(req,res)=>{
+app.get("/x/:num1/y/:num2",(req,res)=>{
     console.log(req.url)
     console.log(req.params)
     res.send((+req.params.id + +req.params.er).toString())
