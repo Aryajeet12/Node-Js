d863c39269d542519cf5fd7c6b661afa1fa94766
Aryaeet12 <aryajeetkumar34@gmail.com>
2026-02-19 11:32:10 +0530

    update

---
+++ index3.js
@@ -0,0 +1,14 @@
+import express from 'express'
+const app=express()
+app.listen(8080,()=>{
+    console.log("Server Started")
+})
+app.get("/",(req,res)=>{
+    console.log(req.url)
+    res.send("response form server /")
+})
+
+app.get("/home",(req,res)=>{
+    console.log(req.url)
+    res.send("response form home /")
+})
