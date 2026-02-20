9d2a6d538eaae8c82259ba1d68fe82d467a035b4
Aryaeet12 <aryajeetkumar34@gmail.com>
2026-02-20 11:16:24 +0530

    update

--- index4a.js
+++ index4a.js
@@ -3,12 +3,13 @@ const app=express()
 app.listen(8080,()=>{
     console.log("Server Started")
 })
+//to add 2 numbers
 app.get("/x/:num1/y/:num2",(req,res)=>{
     console.log(req.url)
     console.log(req.params)
     res.send((+req.params.num1 + +req.params.num2).toString())
 })
-
+//for 3 numbers
 app.get("/x/:num1/y/:num2/z/:num3",(req,res)=>{
     console.log(req.url)
     console.log(req.params)
